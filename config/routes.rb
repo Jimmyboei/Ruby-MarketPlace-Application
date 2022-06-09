f4b788dd28652f0e3a5aca7ac7fc8a88fef096d6
jimmyboei <hellocage@hotmail.com>
2022-06-09 17:29:12 +1000

    addded devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+
   root 'home#index'
   resources :listings
   resources :categories
