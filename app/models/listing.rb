a31bc27ef39b105d2073693c0d134f7ff9679a14
jimmyboei <hellocage@hotmail.com>
2022-07-08 00:20:44 +1000

    add validation for listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,4 +4,6 @@ class Listing < ApplicationRecord
     has_many :messages, dependent: :destroy
     resourcify
     has_one_attached :picture
+    validates :name, presence: true
+    validates :price, numericality: { greater_than_orequal_to: 0 }
 end
